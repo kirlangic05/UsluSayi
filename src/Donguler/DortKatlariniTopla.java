51614fe4927771c6099d338c8daa30e0eb9554fe
Furkan Melih <furkanmelihkirlangic@gmail.com>
2023-02-21 22:52:03 +0300

    4e bölünen sayıların toplamı eklendi

---
+++ src/Donguler/DortKatlariniTopla.java
@@ -0,0 +1,28 @@
+package Donguler;
+
+import java.util.Scanner;
+
+public class DortKatlariniTopla {
+    public static void main(String[] args) {
+        int toplam = 0;
+        double a;
+        Scanner inp = new Scanner(System.in);
+
+       do {
+           System.out.print("Sayı Giriniz : ");
+           a = inp.nextDouble();
+           if (a % 2 == 0) {
+               if (a%4==0){
+                   toplam += a;
+               }
+           }else
+               System.out.println("Lütfen tek sayı girişi yapmayınız.");
+           System.out.println(toplam);
+       }
+        while ((a % 2 == 0));
+
+
+
+
+    }
+}
